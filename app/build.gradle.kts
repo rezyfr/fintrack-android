f04d1960b2689b311ac52e335cda0132f98f0c96
Fidriyanto Rizkillah <frizkillah98@gmail.com>
2026-05-11 04:19:19 +0700

    fix: add kotlin.plugin.compose for Kotlin 2.0 Compose compiler support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
 }
@@ -22,7 +23,6 @@ android {
         }
     }
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
