2f429da754a836a5ea3d8501fcd0423155781317
Fidriyanto Rizkillah <frizkillah98@gmail.com>
2026-05-11 04:34:33 +0700

    fix: targetSdk 35, disable allowBackup, add launcher icons, fix NLS annotation

--- app/src/main/java/com/fidriyanto/banktracker/service/BankNotificationService.kt
+++ app/src/main/java/com/fidriyanto/banktracker/service/BankNotificationService.kt
@@ -2,9 +2,7 @@ package com.fidriyanto.banktracker.service
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class BankNotificationService : NotificationListenerService() {
     companion object {
         const val BANGKOK_BANK_PACKAGE = "th.co.bangkokbank.bangkokmobile"
